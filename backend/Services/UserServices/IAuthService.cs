d69cc37b19bf7d60e89e39c4cdbdc1b69de3375a
rafidoth <rafiulhasan803@gmail.com>
2026-07-30 03:05:21 +0600

    Implement AuthService for user registration

---
+++ backend/Services/UserServices/IAuthService.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using server.Dto;
+using server.Entities;
+
+namespace server.Services.UserServices;
+
+public interface IAuthService
+{
+    Task<RegisterResponse> RegisterAsync(RegisterDto request);
+    Task<ApplicationUser?> GetUserByEmailAsync(string email);
+    Task<IdentityResult> CreateNewUserAsync(ApplicationUser user, string password);
+    Task<IdentityResult> AssignRoleAsync(ApplicationUser user, string role);
+    Task<SignInResult> SignInUserAsync(ApplicationUser user, string password, bool isPersistent);
+}
