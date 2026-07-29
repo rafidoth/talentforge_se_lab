be559da2dcca78dbc3c0084b5e614f887588d87c
rafidoth <rafiulhasan803@gmail.com>
2026-07-29 21:52:46 +0600

    Add database registration extension method

---
+++ backend/Extensions/EfCoreExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+
+namespace server.Extensions;
+
+public static class EfCoreExtensions
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+        
+        return services;
+    }
+}
--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9fb6a28931db8246c29773838642d8efc10933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1023538235e904960bedc0dc86297c187d1578ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
