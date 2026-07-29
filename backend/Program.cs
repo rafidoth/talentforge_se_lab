e70d30b4add8cb2b15234e2bf6f5b98a89c74cba
rafidoth <rafiulhasan803@gmail.com>
2026-07-29 22:20:46 +0600

    Configure authentication and authorization in pipeline

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,6 +4,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddDatabase(builder.Configuration);
+builder.Services.AddIdentityHandlersAndStores();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.Run();
