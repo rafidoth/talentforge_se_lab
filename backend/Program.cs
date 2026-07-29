bff2e1c7d60a846a67a3db9ce8e1da2346e425ba
rafidoth <rafiulhasan803@gmail.com>
2026-07-29 21:57:46 +0600

    Hook up database context in Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,9 @@
+using server.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddDatabase(builder.Configuration);
+
 var app = builder.Build();
 app.Run();
