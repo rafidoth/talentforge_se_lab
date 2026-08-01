e387b6a543840be272d0e9f347a826880effba4d
rafidoth <rafiulhasan803@gmail.com>
2026-08-01 22:47:36 +0600

    Add ServiceResult and PagedResponse utility classes

---
+++ backend/Utils/PagedResponse.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Utils;
+
+public class PagedResponse<T>
+{
+    public IReadOnlyList<T> Data { get; init; } = [];
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public int TotalPages { get; init; }
+    public int TotalRecords { get; init; }
+    public bool HasNextPage => PageNumber < TotalPages;
+    public bool HasPreviousPage => PageNumber > 1;
+}
+
+public static class PagedResponse
+{
+    public static async Task<PagedResponse<T>> CreateAsync<T>(
+        IQueryable<T> query,
+        int pageNumber,
+        int pageSize,
+        int maxPageSize = 50)
+    {
+        pageNumber = Math.Max(1, pageNumber);
+        pageSize = Math.Clamp(pageSize, 1, maxPageSize);
+
+        var totalRecords = await query.CountAsync();
+        var totalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
+
+        if (totalPages > 0)
+        {
+            pageNumber = Math.Min(pageNumber, totalPages);
+        }
+
+        var items = await query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return new PagedResponse<T>
+        {
+            Data = items,
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalPages = totalPages,
+            TotalRecords = totalRecords
+        };
+    }
+}
