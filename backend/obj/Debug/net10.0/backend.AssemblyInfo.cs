465ba3b7073185d10fb2dff600b55da9a0a08204
rafidoth <rafiulhasan803@gmail.com>
2026-08-06 00:32:57 +0600

    Implement pagination execution logic (Skip, Take, CountAsync)

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+613e475e7c24011d12720df3fcb887d98f83e50e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82975addca9fb427753d89acaa0f539099e3a1ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
