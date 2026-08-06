81cd97c7eb594cd00d130738f2519123231e2b04
rafidoth <rafiulhasan803@gmail.com>
2026-08-07 02:36:38 +0600

    Add data mapping from ProfileAttribute to ProfileAttributeDto

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f23d356591f95e79da984383991afeef59e518")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3afc184be465779b3237107ace20a1164f00fa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
