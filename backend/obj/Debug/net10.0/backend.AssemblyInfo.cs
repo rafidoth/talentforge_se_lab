b338ce963a1c0319bebdc75100b5009203cfa17c
rafidoth <rafiulhasan803@gmail.com>
2026-08-12 02:36:42 +0600

    Implement pagination wrapping for project search results

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16db01f77502d6735e0ed0d0b7773eebdf773d71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a67a40dc75203b6054bc7393b8733d24da663e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
