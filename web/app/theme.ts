c3794964699c09c84c111e8fc7624e190bb5f7ea
yeasin-nur <yeasinnurrr@gmail.com>
2026-08-11 11:46:01 +0600

    Update configuration

---
+++ web/app/theme.ts
@@ -0,0 +1,16 @@
+import { closeOnEscape, type MantineThemeOverride } from '@mantine/core';
+
+const theme: MantineThemeOverride = {
+    colors: {
+        dark: [
+            '#ffffff',
+            '#aaa2a4',
+            '#817b7d',
+            '#575555',
+            '#2f2e2e',
+            '#2e2e2e',
+            '#272526',
+            '#1f1d1d',
+            '#161515',
+            '#0f0c0d'
+        ],
