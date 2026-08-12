1110e3ddff206ce0447493181c087d52437b53ce
yeasin-nur <yeasinnurrr@gmail.com>
2026-08-12 17:54:12 +0600

    Improve performance

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -14,3 +14,19 @@ import {
   MantineProvider,
   createTheme,
   type MantineColorsTuple,
+  mantineHtmlProps
+} from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
+import { useAuthStore, useCheckAuth } from "./auth/store";
+import theme from "./theme";
+export const links: Route.LinksFunction = () => [
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
