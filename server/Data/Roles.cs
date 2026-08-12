5617fb672dfcdb2a7234916156a8fb8cf4d61afc
rafidoth <rafiulhasan803@gmail.com>
2026-08-12 13:57:50 +0600

    Add Roles entity and domain models

---
+++ server/Data/Roles.cs
@@ -0,0 +1,12 @@
+﻿namespace server.Data;
+
+public static class Roles
+{
+    public const string Admin = "Administrator";
+    public const string Candidate = "Candidate";
+    public const string Recruiter = "Recruiter";
+
+    public const string AdminOrRecruiter = Admin + "," + Recruiter;
+    public const string AdminOrCandidate = Admin + "," + Candidate;
+
+}
