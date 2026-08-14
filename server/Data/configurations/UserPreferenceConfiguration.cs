41b58f020040a0c3f8e0d4541aa70971756c1cf3
rafidoth <rafiulhasan803@gmail.com>
2026-08-14 22:36:11 +0600

    Add EF Core configuration for UserPreference

---
+++ server/Data/configurations/UserPreferenceConfiguration.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using server.Entities;
+
+namespace server.Data.configurations
+{
+    public class UserPreferenceConfiguration : IEntityTypeConfiguration<UserPreference>
+    {
+        public void Configure(EntityTypeBuilder<UserPreference> builder)
+        {
+            builder.Property(a => a.CreatedAt).HasDefaultValueSql("now()").ValueGeneratedOnAdd();
+        }
+    }
+}
