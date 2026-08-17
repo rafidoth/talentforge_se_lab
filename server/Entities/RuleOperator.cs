31d707b4ece72dec819947089a58dfafffd4e6b0
rafidoth <rafiulhasan803@gmail.com>
2026-08-17 08:03:05 +0600

    Add RuleOperator entity and domain models

---
+++ server/Entities/RuleOperator.cs
@@ -0,0 +1,13 @@
+namespace server.Entities
+{
+    public enum RuleOperator
+    {
+        Equals,
+        NotEquals,
+        GreaterThan,
+        GreaterThanOrEqual,
+        LessThan,
+        LessThanOrEqual,
+        Contains
+    }
+}
