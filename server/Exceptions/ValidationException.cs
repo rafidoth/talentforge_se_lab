61b60966f2437ca36d2ac464bcc857560eab31ee
rafidoth <rafiulhasan803@gmail.com>
2026-08-18 23:56:00 +0600

    Add custom exception ValidationException

---
+++ server/Exceptions/ValidationException.cs
@@ -0,0 +1,23 @@
+using System.Net;
+
+namespace server.Exceptions;
+
+public sealed class ValidationException : AppException
+{
+    public IDictionary<string, string[]> Errors { get; }
+
+    public ValidationException(IDictionary<string, string[]> errors)
+        : base("One or more validation errors occurred.", HttpStatusCode.BadRequest)
+    {
+        Errors = errors;
+    }
+
+    public ValidationException(string field, string error)
+        : base("One or more validation errors occurred.", HttpStatusCode.BadRequest)
+    {
+        Errors = new Dictionary<string, string[]>
+        {
+            { field, [error] }
+        };
+    }
+}
