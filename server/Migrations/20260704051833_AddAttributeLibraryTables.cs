a5c8db74badbe3873a56aaf9b42aefff12ea54a7
rafidoth <rafiulhasan803@gmail.com>
2026-08-20 07:23:18 +0600

    Add 20260704051833_AddAttributeLibraryTables entity and domain models

---
+++ server/Migrations/20260704051833_AddAttributeLibraryTables.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAttributeLibraryTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AttributeCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttributeCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AttributeTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttributeTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Projects_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TechnologyTags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TechnologyTags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attributes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "jsonb", nullable: false),
+                    TypeId = table.Column<int>(type: "integer", nullable: true),
+                    CategoryId = table.Column<int>(type: "integer", nullable: true),
+                    IsBuiltin = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attributes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attributes_AttributeCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "AttributeCategories",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Attributes_AttributeTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "AttributeTypes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectTechnologyTags",
+                columns: table => new
+                {
+                    ProjectId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TagId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectTechnologyTags", x => new { x.ProjectId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_ProjectTechnologyTags_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProjectTechnologyTags_TechnologyTags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "TechnologyTags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AttributeDropdownOptions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AttributeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Label = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttributeDropdownOptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AttributeDropdownOptions_Attributes_AttributeId",
+                        column: x => x.AttributeId,
+                        principalTable: "Attributes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProfileAttributes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    AttributeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Value = table.Column<string>(type: "jsonb", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProfileAttributes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProfileAttributes_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProfileAttributes_Attributes_AttributeId",
+                        column: x => x.AttributeId,
+                        principalTable: "Attributes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AttributeCategories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Personal Information" },
+                    { 2, "Certifications" },
+                    { 3, "Domain Knowledge" },
+                    { 4, "Soft Skills" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AttributeTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "String" },
+                    { 2, "Text" },
+                    { 3, "Image" },
+                    { 4, "Numeric" },
+                    { 5, "Date" },
+                    { 6, "Period" },
+                    { 7, "Boolean" },
+                    { 8, "One of Many" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttributeCategories_Name",
+                table: "AttributeCategories",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttributeDropdownOptions_AttributeId",
+                table: "AttributeDropdownOptions",
+                column: "AttributeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attributes_CategoryId",
+                table: "Attributes",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attributes_Name",
+                table: "Attributes",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attributes_TypeId",
+                table: "Attributes",
+                column: "TypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttributeTypes_Name",
+                table: "AttributeTypes",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfileAttributes_AttributeId",
+                table: "ProfileAttributes",
+                column: "AttributeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfileAttributes_UserId_AttributeId",
+                table: "ProfileAttributes",
+                columns: new[] { "UserId", "AttributeId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_UserId",
+                table: "Projects",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectTechnologyTags_TagId",
+                table: "ProjectTechnologyTags",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TechnologyTags_Name",
+                table: "TechnologyTags",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AttributeDropdownOptions");
+
+            migrationBuilder.DropTable(
+                name: "ProfileAttributes");
+
+            migrationBuilder.DropTable(
+                name: "ProjectTechnologyTags");
+
+            migrationBuilder.DropTable(
+                name: "Attributes");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "TechnologyTags");
+
+            migrationBuilder.DropTable(
+                name: "AttributeCategories");
+
+            migrationBuilder.DropTable(
+                name: "AttributeTypes");
+        }
+    }
+}
