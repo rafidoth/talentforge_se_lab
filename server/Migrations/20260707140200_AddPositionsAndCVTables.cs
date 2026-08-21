f6fd0d7617ce497e68aac5c4b6166fb5ab0d72cd
rafidoth <rafiulhasan803@gmail.com>
2026-08-21 20:40:07 +0600

    Add 20260707140200_AddPositionsAndCVTables entity and domain models

---
+++ server/Migrations/20260707140200_AddPositionsAndCVTables.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPositionsAndCVTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    ShortDescription = table.Column<string>(type: "text", nullable: true),
+                    IsPublic = table.Column<bool>(type: "boolean", nullable: false),
+                    MaxProjects = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cvs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CandidateId = table.Column<string>(type: "text", nullable: false),
+                    PositionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    LikeCount = table.Column<int>(type: "integer", nullable: false),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cvs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cvs_AspNetUsers_CandidateId",
+                        column: x => x.CandidateId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cvs_Positions_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Positions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PositionAccessRules",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PositionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AttributeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Operator = table.Column<string>(type: "text", nullable: false),
+                    ExpectedValue = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PositionAccessRules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PositionAccessRules_Attributes_AttributeId",
+                        column: x => x.AttributeId,
+                        principalTable: "Attributes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PositionAccessRules_Positions_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Positions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PositionAttributes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PositionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AttributeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Order = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PositionAttributes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PositionAttributes_Attributes_AttributeId",
+                        column: x => x.AttributeId,
+                        principalTable: "Attributes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PositionAttributes_Positions_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Positions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PositionDiscussionPosts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PositionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AuthorId = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PositionDiscussionPosts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PositionDiscussionPosts_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PositionDiscussionPosts_Positions_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Positions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PositionTechnologyTags",
+                columns: table => new
+                {
+                    PositionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TagId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PositionTechnologyTags", x => new { x.PositionId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_PositionTechnologyTags_Positions_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Positions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PositionTechnologyTags_TechnologyTags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "TechnologyTags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CvLikes",
+                columns: table => new
+                {
+                    CvId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RecruiterId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CvLikes", x => new { x.CvId, x.RecruiterId });
+                    table.ForeignKey(
+                        name: "FK_CvLikes_AspNetUsers_RecruiterId",
+                        column: x => x.RecruiterId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CvLikes_Cvs_CvId",
+                        column: x => x.CvId,
+                        principalTable: "Cvs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CvLikes_RecruiterId",
+                table: "CvLikes",
+                column: "RecruiterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cvs_CandidateId_PositionId",
+                table: "Cvs",
+                columns: new[] { "CandidateId", "PositionId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cvs_PositionId",
+                table: "Cvs",
+                column: "PositionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PositionAccessRules_AttributeId",
+                table: "PositionAccessRules",
+                column: "AttributeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PositionAccessRules_PositionId",
+                table: "PositionAccessRules",
+                column: "PositionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PositionAttributes_AttributeId",
+                table: "PositionAttributes",
+                column: "AttributeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PositionAttributes_PositionId_AttributeId",
+                table: "PositionAttributes",
+                columns: new[] { "PositionId", "AttributeId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PositionDiscussionPosts_AuthorId",
+                table: "PositionDiscussionPosts",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PositionDiscussionPosts_PositionId",
+                table: "PositionDiscussionPosts",
+                column: "PositionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PositionTechnologyTags_TagId",
+                table: "PositionTechnologyTags",
+                column: "TagId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CvLikes");
+
+            migrationBuilder.DropTable(
+                name: "PositionAccessRules");
+
+            migrationBuilder.DropTable(
+                name: "PositionAttributes");
+
+            migrationBuilder.DropTable(
+                name: "PositionDiscussionPosts");
+
+            migrationBuilder.DropTable(
+                name: "PositionTechnologyTags");
+
+            migrationBuilder.DropTable(
+                name: "Cvs");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+        }
+    }
+}
