7a21c23592d4fb741fe3bdeaf137618fc73cf3df
rafidoth <rafiulhasan803@gmail.com>
2026-08-23 00:18:03 +0600

    Add 20260716204401_AddProjectIdsToCV entity and domain models

---
+++ server/Migrations/20260716204401_AddProjectIdsToCV.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectIdsToCV : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Positions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "ChosenProjectIds",
+                table: "Cvs",
+                type: "uuid[]",
+                nullable: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Cvs",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Positions");
+
+            migrationBuilder.DropColumn(
+                name: "ChosenProjectIds",
+                table: "Cvs");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Cvs");
+        }
+    }
+}
