86db6205fa863d55a8bec75c5ac6fb27c2651952
rafidoth <rafiulhasan803@gmail.com>
2026-08-24 10:25:09 +0600

    Add 20260730133203_AddSalesforceContactIdToUser entity and domain models

---
+++ server/Migrations/20260730133203_AddSalesforceContactIdToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSalesforceContactIdToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SalesforceContactId",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SalesforceContactId",
+                table: "AspNetUsers");
+        }
+    }
+}
