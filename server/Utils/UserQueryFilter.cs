db47a7d6e64d67cdf3210244c202d8b566d4c0e3
rafidoth <rafiulhasan803@gmail.com>
2026-08-28 13:16:59 +0600

    Add UserQueryFilter entity and domain models

---
+++ server/Utils/UserQueryFilter.cs
@@ -0,0 +1,10 @@
+namespace server.Services.UserServices
+{
+    public class UserQueryFilter
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? Search { get; set; }
+        public string? SortBy { get; set; }
+    }
+}
