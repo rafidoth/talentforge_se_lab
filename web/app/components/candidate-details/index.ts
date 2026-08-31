a991ec707154bbbaf206ee96b2d59b16b538fe75
Md Yeasin Nur Rahman <yeasinnurrr@gmail.com>
2026-09-01 05:05:39 +0600

    style: format and clean up index.ts

---
+++ web/app/components/candidate-details/index.ts
@@ -0,0 +1,5 @@
+export { CandidateProfileHeader } from './CandidateProfileHeader';
+export { CandidateProfileAttributes } from './CandidateProfileAttributes';
+export { CandidateProfileProjects } from './CandidateProfileProjects';
+export { CandidateProfileCvs } from './CandidateProfileCvs';
+
