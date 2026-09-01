992cc82e69f9cbfcf8da10e9cb03c8e981f5fb3e
Md Yeasin Nur Rahman <yeasinnurrr@gmail.com>
2026-09-02 02:39:36 +0600

    feat(ui): add index.ts component

---
+++ web/app/components/attributeLibrary/index.ts
@@ -0,0 +1,3 @@
+export { AttributeLibraryModal } from "./AttributeLibraryModal";
+export type { AttributeLibraryModalProps } from "./AttributeLibraryModal";
+export { BaseAttributeList } from "./BaseAttributeList";
