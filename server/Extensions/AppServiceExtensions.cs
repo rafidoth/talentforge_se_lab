1f060283a0f8e49ebe682edb6acf16ade01a89bf
rafidoth <rafiulhasan803@gmail.com>
2026-09-06 07:48:55 +0600

    Refactor service methods in AppServiceExtensions.cs

--- server/Extensions/AppServiceExtensions.cs
+++ server/Extensions/AppServiceExtensions.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Identity;
 using server.Entities;
 using server.Services.AttributeLibraryServices;
+using server.Services.PositionServices;
 using server.Services.ProfileServices;
 using server.Services.ProjectsServices;
 using server.Services.TagsServices;
 using server.Services.UserServices;
+using server.Services.CvServices;
 using server.Services.CloudinaryServices;
+using server.Services.SalesforceServices;
 using server.Utils;
 
 namespace server.Extensions
@@ -19,10 +22,16 @@ namespace server.Extensions
             services.AddScoped<IProfileService, ProfileService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IUserConfirmation<ApplicationUser>, UserConfirmation>();
+            services.AddScoped<IPositionService, PositionService>();
+            services.AddScoped<IPositionAttributeService, PositionAttributeService>();
+            services.AddScoped<IPositionTagsService, PositionTagsService>();
+            services.AddScoped<IPositionAccessRuleService, PositionAccessRuleService>();
             services.AddScoped<IProjectsService, ProjectsService>();
             services.AddScoped<ITagsService, TagsService>();
+            services.AddScoped<ICvService, CvService>();
             services.AddScoped<ICloudinaryService, CloudinaryService>();
             services.AddScoped<IUserPreferenceService, UserPreferenceService>();
+            services.AddScoped<ISalesforceService, SalesforceService>();
             return services;
         }
     }
--- server/Services/ProfileServices/ProfileService.GetCandidateFullProfile.cs
+++ server/Services/ProfileServices/ProfileService.GetCandidateFullProfile.cs
@@ -8,13 +8,15 @@ namespace server.Services.ProfileServices
 
         public async Task<CandidateFullProfileDto> GetCandidateFullProfileAsync(ApplicationUser user)
         {
+            var allCvs = await cvService.GetAllCvsByCandidateIdAsync(user.Id);
             return new CandidateFullProfileDto
             {
                 CandidateId = user.Id,
                 InfoSection = BuildInfoSection(user),
                 MeSection = await GetMeSectionAsync(user.Id),
                 Attributes = await GetNonBuiltInAttributesAsync(user.Id),
-                Projects = await projectsService.GetAllProjectsByUserAsync(user.Id)
+                Projects = await projectsService.GetAllProjectsByUserAsync(user.Id),
+                Cvs = allCvs.Where(cv => cv.IsPublished == true).ToList()
             };
         }
 
