e85ac17bbbe469172169bf379845095c3ee23450
rafidoth <rafiulhasan803@gmail.com>
2026-09-10 14:32:51 +0600

    Improve UI component AccessRulesTable.tsx

--- web/app/components/positions/positionDetails/AccessRulesModal/AccessRulesTable.tsx
+++ web/app/components/positions/positionDetails/AccessRulesModal/AccessRulesTable.tsx
@@ -54,4 +54,3 @@ export function AccessRulesTable({ rules, operatorLabels, onAddRule, onDeleteRul
     </>
   );
 }
-
--- web/app/components/positions/positionDetails/OverviewTab.tsx
+++ web/app/components/positions/positionDetails/OverviewTab.tsx
@@ -56,4 +56,3 @@ export function OverviewTab({ positionId, position }: OverviewTabProps) {
   );
 }
 
-
